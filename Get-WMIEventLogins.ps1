8a3237d47db6ff3c1f41f956d289ba2ef33e045b
epena <evan.pena@mandiant.com>
2016-12-14 10:22:42 -0800

    fixed targets parameter to target

--- Get-WMIEventLogins.ps1
+++ Get-WMIEventLogins.ps1
@@ -6,7 +6,7 @@ Will get remote login details from event log on remote hosts.
 This can be used to find out where people are logging in from or
 to find jump boxes.
 
-.PARAMETER Targets
+.PARAMETER Target
 List of targets. Will accept value from pipe.
 
 .PARAMETER User
